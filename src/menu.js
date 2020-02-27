6ec94d73f88342d6f259dc402ba24c5d2cc7d646
Edd Sansome <edd@murismo.com>
2020-02-27 16:19:57 +0000

    Add initial dishes to menu

--- src/menu.js
+++ src/menu.js
@@ -4,8 +4,6 @@ const menu = () => {
   let menuGrid = document.createElement('div');
   menuGrid.id = 'menuGrid';
   menuNode.appendChild(menuGrid);
-  
-
 
 };
 
@@ -14,7 +12,39 @@ const menuContents = () => {
   const contentNode = document.getElementById('menuGrid');
   let contentGrid = document.createElement('div');
   contentGrid.id = 'contentGrid';
-  contentGrid.innerHTML = 'menu';
-  contentNode.appendChild(contentGrid);
+
+  const meal = (name, description, price) => {
+  
+    return {name:name, description: description, price: price}
+  }
+
+const menu = [
+  meal('Fish and Chips', 'Yummy!','£10.50'),
+  meal('Gammon and Eggs', 'Noice!', '£9.50'),
+  meal('Jacket Potato', 'The Classic', '£6.50'),
+]
+
+menu.forEach((e) => {
+  
+  let mealNode = document.createElement('ul');
+  let nameOfDish = document.createElement('li');
+  let descriptionOfDish = document.createElement('li');
+  let priceOfDish = document.createElement('li');
+
+  nameOfDish.innerHTML = e.name;
+  descriptionOfDish.innerHTML = e.description;
+  priceOfDish.innerHTML = e.price;
+
+  mealNode.appendChild(nameOfDish);
+  mealNode.appendChild(descriptionOfDish);
+  mealNode.appendChild(priceOfDish);
+  contentNode.appendChild(mealNode);  
+
+})
+
+
+ //hardcoded test//
+//  contentGrid.innerHTML = `${menu[0].name} ${menu[0].description} ${menu[0].price}`;
+//  contentNode.appendChild(contentGrid);
 }
 export { menu, menuContents }
---
+++ src/menuList.js
@@ -0,0 +1,6 @@
+
+const menu = [
+  meal('Fish and Chips', 'Yummy!','£10.50'),
+  meal('Gammon and Eggs', 'Noice!', '£9.50'),
+  meal('Jacket Potato', 'The Classic', '£6.50'),
+]
